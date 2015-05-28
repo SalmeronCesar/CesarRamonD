105c814afb411cdeb3c0cab17b4df095430ab550
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2015-05-28 14:06:19 -0700

    final

--- index.php
+++ index.php
@@ -26,53 +26,82 @@
                 </div>
             </div>
         </div>
-<!--        <div id="nav">
-            The links to login.php, index.php, and to logout-user.php
-                <a href="motors.php">Motor</a><a href="fashion.php">Fashion</a><a href="sportinggoods.php">Sporting Goods</a><a href="Home&Garden.php">Home&Garden</a><a href="Electronics.php">Electronics</a><a href="Jewelry.php">Jewelry</a>
-                 end navmenu -->
-                <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="http://www.mikefordmustang.com/pictures/front_web.jpg" data-slide-to="0" class="active"></li>
-    <li data-target="http://images.menswearhouse.com/is/image/TMW/MW40_359F_67_MICHAEL_KORS_BLUE_PLAID_SET?wid=1200&hei=627" data-slide-to="1"></li>
-    <li data-target="https://silverrockfunding.com/images/sporting_goods_store.jpg" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
-  </ol>
+               <title>Carousel</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 70%;
+      margin: auto;
+  }
+  </style>
+</head>
+<body>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="http://www.mikefordmustang.com/pictures/front_web.jpg" alt="Chania">
+<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="http://autocarsblitz.com/wp-content/uploads/2015/02/Front-Angle-2015-BMW-M5-Sedan-Wallpaper.jpg" data-slide-to="0" class="active"></li>
+      <li data-target="http://images.menswearhouse.com/is/image/TMW/MW40_359F_67_MICHAEL_KORS_BLUE_PLAID_SET?wid=1200&hei=627" data-slide-to="1"></li>
+      <li data-target="https://silverrockfunding.com/images/sporting_goods_store.jpg" data-slide-to="2"></li>
+      <li data-target="http://upload.wikimedia.org/wikipedia/commons/a/a9/20090809_Lotus_flower_2736.jpg" data-slide-to="4"></li>
+      
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+          <!-- Is the 1st and active item -->
+        <img src="http://autocarsblitz.com/wp-content/uploads/2015/02/Front-Angle-2015-BMW-M5-Sedan-Wallpaper.jpg" alt="Chania" width="460" height="345">
+      </div>
+<!-- Is the 2nd item on the slide-->
+      <div class="item">
+        <img src="http://images.menswearhouse.com/is/image/TMW/MW40_359F_67_MICHAEL_KORS_BLUE_PLAID_SET?wid=1200&hei=627" alt="Chania" width="460" height="345">
+      </div>
+    <!-- Is the 3rd item on the slide -->
+      <div class="item">
+        <img src="https://silverrockfunding.com/images/sporting_goods_store.jpg" alt="Flower" width="460" height="345">
+      </div>
+<!-- Is the 4th item on the slide -->
+      <div class="item">
+        <img src="http://creativegemsinc.com/wp-content/uploads/2013/09/filepicker-psS2qFyWTwaWYZNgYz5G_Rolex.jpg" alt="Flower" width="460" height="345">
+      </div>
+        <!-- Is the last item on the slide -->
+         <div class="item">
+        <img src="http://upload.wikimedia.org/wikipedia/commons/a/a9/20090809_Lotus_flower_2736.jpg" alt="Flower" width="460" height="345">
+      </div>
     </div>
 
-    <div class="item">
-      <img src="http://images.menswearhouse.com/is/image/TMW/MW40_359F_67_MICHAEL_KORS_BLUE_PLAID_SET?wid=1200&hei=627" alt="Chania">
-    </div>
-
-    <div class="item">
-      <img src="https://silverrockfunding.com/images/sporting_goods_store.jpg" alt="Flower">
-    </div>
-
-    <div class="item">
-      <img src="img_flower2.jpg" alt="Flower">
-    </div>
+    <!-- Left and right controls -->
+    <!-- Makes you go left which is previous -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a><!-- Makes you go right which is the next slide -->
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-
                 <details>
-  <summary>Details and Price</summary>
-  <p>2006 Mustang Cobra </p>
-  <p>Asking $12k</p>
+  <summary>Item Description and Price</summary>
+  <p>2015 BMW</p>
+  <p>$30k and firm on price</p>
+  <p>Michael Kors Suit</p>
+  <p>Selling a Michael Kors suit for $600 OBO</p>
+  <p>Sporting Goods</p>
+  <p>Take all for $50</p>
+  <p>Rolex Watch</p>
+  <p>Ill take $400</p>
+  <p>Flower</p>
+  <p>$5</p>
 </details>
     </body>
     </html>
